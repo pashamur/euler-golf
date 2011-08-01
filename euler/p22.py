3f1fa72a6fb862f8a3413ae1784837b68c943467
Pasha Muravyev <pashamur@gmail.com>
2011-07-31 22:53:52 -0400

    seven lines

--- euler/p22.py
+++ euler/p22.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 import os
 os.system('perl -ne \'s/\"//g;@e=split(/,/);foreach(@e){print $_.qq(\n);}\' names.txt > NMS.T')
-(total, i) = (0, 1)
-for line in sorted(open("NMS.T","r").readlines()): 
-	for c in line.rstrip():
-		total+=(ord(c)-ord('A')+1)*i
-	i=i+1
+(total, a) = (0, sorted(open("NMS.T","r").readlines()))
+for line in a: 
+	for c in line.rstrip(): total+=(ord(c)-ord('A')+1)*a.index(line)
 print total
