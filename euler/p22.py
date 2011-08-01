5dc5bea69b5728587b44558372d25382b91f6e9b
hogbait <hogbait@hogbait-server.(none)>
2011-07-31 22:34:51 -0400

    mooooooooooooooo

--- euler/p22.py
+++ euler/p22.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python
-import sys
 import os
 os.system('perl -ne \'s/\"//g;@e=split(/,/);foreach(@e){print $_.qq(\n);}\' names.txt > NMS.T')
-def compute(l):
-	tot=0
-	for c in l.rstrip(): tot+=(ord(c)-ord('A')+1)
-	return tot
 (total, i) = (0, 1)
 for line in sorted(open("NMS.T","r").readlines()): 
-	total += i*compute(line)
+	for c in line.rstrip():
+		total+=(ord(c)-ord('A')+1)*i
 	i=i+1
 print total
