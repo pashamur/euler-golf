8de1d55137b3ebbd23f18abe4afe7aaf019f6038
Pasha Muravyev <pashamur@gmail.com>
2011-07-31 22:25:17 -0400

    dumb bugfix

--- euler/p22.py
+++ euler/p22.py
@@ -2,13 +2,14 @@
 import sys
 import os
 os.system('perl -ne \'s/\"//g;@e=split(/,/);foreach(@e){print $_.qq(\n);}\' names.txt > NMS.T')
-def compute(l, tot):
+def compute(l):
+	tot=0
 	for c in l.rstrip(): tot+=(ord(c)-ord('A')+1)
 	return tot
 def main():
 	(total, i) = (0, 1)
 	for line in sorted(open("NMS.T","r").readlines()): 
-		total += compute(line,0)
+		total += i*compute(line)
 		i=i+1
 	print total
 if __name__ == "__main__": main()
