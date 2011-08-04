4021520892be10bca50d9547d1bc1c94ff2f15e9
Pasha Muravyev <pashamur@gmail.com>
2011-08-04 12:51:29 -0400

    Problem: 40; Lang:Python; Runtime: 0.188s; Lines: 5; Chars: 174

--- 040/p40.py
+++ 040/p40.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python
-
-import sys
-
-def main():
-	q = ""
-	for i in range(1, 250000):
-		q+=str(i)
-	print int(q[10])
-	print int(q[1-1])*int(q[10-1])*int(q[100-1])*int(q[1000-1])*int(q[10000-1])*int(q[100000-1])*int(q[1000000-1])
-
-
-if __name__ == "__main__":
-	main()
+q = ""
+for i in range(1, 250000):
+	q+=str(i)
+print int(q[1-1])*int(q[10-1])*int(q[100-1])*int(q[1000-1])*int(q[10000-1])*int(q[100000-1])*int(q[1000000-1])
