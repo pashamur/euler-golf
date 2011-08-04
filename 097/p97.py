bd309c9cb4447e9d0f046dcfeb92c617adf41b5b
Pasha Muravyev <pashamur@gmail.com>
2011-08-04 12:46:36 -0400

    Lang:Python; Runtime: 0.121s; Lines: 6; Chars: 128

--- 097/p97.py
+++ 097/p97.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python
-
-import sys
-
-def main():
-	num = 1125899906842624 # == 2**50 
-	q=2
-	for i in range(0, 156609):
-		q *= num
-		q %= 10000000000
-	print 28433*q*2**6+1
-
-if __name__ == "__main__":
-	main()
+(q, num) = (2, 1125899906842624) 
+for i in range(0, 156609):
+	q *= num
+	q %= 10000000000
+print 28433*q*2**6+1
