a51ef5813efbab5409f1947de2d2d9d4104e206a
Pasha Muravyev <pashamur@gmail.com>
2011-08-04 12:11:40 -0400

    Alg:Brute Force; Lang:Python; Runtime: 3.035s; Lines: 17; Chars: 250

---
+++ 030/p30.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+	tot=0
+	for i in range(2,1000000):
+		dgsum = 0
+		for c in str(i):
+			dgsum += (ord(c)-ord('0'))**5
+		if dgsum == i:
+			tot += i
+			print "Found such number:",i
+	print tot
+
+if __name__ == "__main__":
+	main()
